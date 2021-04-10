569c3b1199103f6f9766243d5e6b7e6ac673a8e2
Ryley Taub <taubr10@gmail.com>
2021-04-10 18:28:55 -0400

    commit

--- as18.php
+++ as18.php
@@ -1,22 +1,73 @@
+<DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <title>As18</title>
+        <meta charset="hutf-8" />
+    </head>
+    
+    <body>
+        <div class = "container">
+        <?php
+        echo "<div class='page-header'>
+                <h1>As18 Covid-19 Deaths api</h1>
+                <a href='https://github.com/rtaub/api'>Github code</a>
+            </div>";
+        ?>
+                         
 <?php
-echo "<a target='_blank' href='https://github.com/rtaub/api.git'>Github Repo</a>";
+
+// Calls main so the table can be displayed/printed
 main();
 
-function main(){
-    $apiCall = 'https://api.covid19api.com/summary';
-    $json_string = curl_get_contents($apiCall);
-    $obj = json_decode($json_string);
-
+function main () {
+    // Covid19api.com deaths data
+	$apiCall = 'https://api.covid19api.com/summary';
+    // Reads the data from the website and puts it into a JSON string
+	$json_string = curl_get_contents($apiCall);
+    // Stores the JSON string into an object
+	$obj = json_decode($json_string);
+	//arrays to hold both countries, and their deaths
     $arr1 = Array();
-    $arr2 = Array();
-    foreach($obj->Countries as $i){
-        array_push($arr1, $i->Country);
-        array_push($arr2, $i->TotalDeaths);
-    }
-    array_multisort($arr2, SORT_DESC, $arr1);
+	$arr2 = Array();
+	
+	foreach($obj->Countries as $i)
+	{
+		array_push($arr1, $i->Country );
+		array_push($arr2, $i->TotalDeaths );
+	}
+	//sort the arrays descending 
+	array_multisort($arr2, SORT_DESC, $arr1);
+    
+    //generate json object with the countries with the top 10 highest numer of deaths and print it
+    $arr1 = array_slice($arr1, 0, 10);
+    $arr1 = json_encode($arr1);
+    $arr1 = json_decode($arr1);
+    echo "<h4><b>JSON object</b></h4>";
     print_r($arr1);
+    
+    // Create the header
+	echo "<div><h3><b>10 Countries with highest covid-19 deaths</b></h3>";
+    // Create the table
+	echo "<table class='table'>";
+        echo "<tr>";
+            // Create the column headers
+            echo "<th>Country Name</th>";
+            echo "<th>Number of Deaths</th>";
+		echo "</tr>";
+
+        // For loop to add the top 10 countries-arr1 (and their deaths-arr2) to the table 
+		for ($x = 0; $x < 10 ; $x++) {
+			echo "<tr>";
+			echo "<td>{$arr1[$x]}</td>";
+			echo "<td>{$arr2[$x]}</td>";
+			echo "</tr>";
+		 }
+
+	echo "</table>";
+	echo '</div>';
 }
 
+// read data from a URL into a string
 function curl_get_contents($url) {
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
@@ -27,5 +78,3 @@ function curl_get_contents($url) {
     curl_close($ch);
     return $data;
 }
-?>
-
